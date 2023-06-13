8497f153491d3629d3c9a6f1a84da5d4f1179590
rbiomete <100229496+rbiomete@users.noreply.github.com>
2023-06-14 03:32:01 +0400

    fixes module storage output

--- modules/storage-configuration/outputs.tf
+++ modules/storage-configuration/outputs.tf
@@ -2,9 +2,7 @@ output "container_name" {
   description = "A container name to be created. Lakehouse data will be stored in this container"
   value       = azurerm_storage_container.main.name
 }
- 
-#  output "storage_account_name" {
-#    description = "New created Storage account name"
-#    value       = azurerm_storage_account.module_strg_acct[0].name
-   
-#  }
+ output "storage_account_name" {
+  description = "Newly created Storage account name"
+  value       = length(azurerm_storage_account.module_strg_acct) > 0 ? azurerm_storage_account.module_strg_acct[0].name : null
+}
